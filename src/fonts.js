18af2427b6466ad8f7c2bd2fa7edce474fa8525a
舟木正憲 <funakimfunaki@gmail.com>
2019-01-01 21:07:43 +0900

    初期設定

---
+++ src/fonts.js
@@ -0,0 +1,15 @@
+const notoSansThin    = require('../assets/fonts/NotoSansJP-Thin.otf');
+const notoSansLight   = require('../assets/fonts/NotoSansJP-Light.otf');
+const notoSansRegular = require('../assets/fonts/NotoSansJP-Regular.otf');
+const notoSansMedium  = require('../assets/fonts/NotoSansJP-Medium.otf');
+const notoSansBold    = require('../assets/fonts/NotoSansJP-Bold.otf');
+
+const fonts = {
+  'noto-sans-thin'   : notoSansThin,
+  'noto-sans-light'  : notoSansLight,
+  'noto-sans-regular': notoSansRegular,
+  'noto-sans-medium' : notoSansMedium,
+  'noto-sans-bold'   : notoSansBold,
+};
+
+export default fonts;
